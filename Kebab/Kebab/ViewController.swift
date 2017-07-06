8e4f5359a2e9992851e708ce3fdca44e11bb8132
Chenguang Zhou <chen@drawboard.com>
2017-07-06 17:52:32 +1000

    initial commit

---
+++ Kebab/Kebab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kebab
+//
+//  Created by Chenguang Zhou on 5/7/17.
+//  Copyright © 2017 Chenguang Zhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
